f799b54c4b16b7d58bb2de784769c548be5f5548
trlee02 <tristan.rene.lee@gmail.com>
2022-03-28 11:15:25 -0700

    final revision

--- PythonPDF.py
+++ PythonPDF.py
@@ -1,52 +1,47 @@
+# Functions for filling in Excel Template files used for Alpha ATE LabVIEW
+# softwares
+# Date: March 28th, 2022
+# Written By: Tristan Lee
 from openpyxl import load_workbook
 import win32com.client
 
+
 # Fills in ATE excel template of file type .xlsx
-# elements of data_names are the contents of the cells that are to be replaced by elements of the same indeces in data_values
+# requires: results of test_names are at corresponding indeces in test_results
+def write_results(template_path, results_path, test_names, test_results):
 
-
-def write_results(template_path, results_path, data_names, data_values):
+    # loading template file
     report = load_workbook(template_path)
-    report.template = False
-    sheet = report["A5 form"]
+    report.template = False  # this is needed to properly save an xlsx file
+    sheet = report["A5 form"]  # selecting the sheet within the workbook
 
-    i = 0
+    # iterates through spreadsheet cells
     for r in range(2, sheet.max_row+1):
         for c in range(3, sheet.max_column+1):
-            val = sheet.cell(r, c).value
-            if val != None:
-                for idx, name in enumerate(data_names):
+            val = sheet.cell(r, c).value  # value of cell
+            if val != None:  # check to ensure the cell is not empty
+                # test_names indeces align with test_results'
+                for idx, name in enumerate(test_names):
                     if name == str(val):
-                        if "." not in data_values[idx]: # this might need to be changed
-                            sheet.cell(r, c).value = data_values[idx]
-                        else:
-                            sheet.cell(r, c).value = float(data_values[idx])
-            i += 1
+                        try:
+                            # conversion necassary for formulae
+                            sheet.cell(r, c).value = float(test_results[idx])
+                        except:
+                            sheet.cell(r, c).value = test_results[idx]
 
-    report.save(results_path)
+    report.save(results_path)  # saves filled in workbook file
 
 
 # Generates a PDF from a given results_path xlsx file
 def generate_pdf(results_path, pdf_path):
+    # opens excel
     excel = win32com.client.Dispatch("Excel.Application")
 
+    # opens excel workbook file
     final_report = excel.Workbooks.Open(results_path)
     sheet_indeces = [1]
 
+    # saves excel file as pdf and closes
     final_report.WorkSheets(sheet_indeces).Select()
     final_report.ActiveSheet.ExportAsFixedFormat(0, pdf_path)
     final_report.Close()
-
-
-# data_names = ['$!Test_Date', '$!Tested_By',
-#               '$!Part_No', '$!6.4.3.', '$!6.5.5.']
-# data_values = ['March 17, 2022', 'Tristan Lee', '123', 'P', '0.0']
-
-# template = r'C:\Users\trist\Desktop\Desktop\UBC Engineering\Coop\Alpha\Work\Python\PyLab_PDF\templates\0100048-A5_R_1.xltx'
-# ex_result = r'C:\Users\trist\Desktop\Desktop\UBC Engineering\Coop\Alpha\Work\Python\PyLab_PDF\templates\results.xlsx'
-# pdf = r'C:\Users\trist\Desktop\Desktop\UBC Engineering\Coop\Alpha\Work\Python\PyLab_PDF\templates\results.pdf'
-# results = ["Tristan Lee", "0100044-001", "123456789", "Power-ATE-BB", "March 16, 2022", "P", "P", "P", "P", "P", "54.00", "0.0", "P", "P",
-#            "P", "P", "58.01", "47.9", "54.08", "-0.3", "1.00", "-0.5", "52.00", "45.00", "=F36*F35", "50.00", "36.00", "P", "XLTX TEST"]
-
-# write_results(template, ex_result, data_names, data_values)
-# generate_pdf(ex_result, pdf)
--- README.txt
+++ README.txt
@@ -9,11 +9,7 @@ Tristan Lee
 Install Instructions:
 1. In the setup folder and run 
 dependencies.bat
-2. In the setup folder install
-Python 3.6
-	i) IMPORTANT: click add
-	to PATH, when installing
-3. Open PythonPDF.vi
+2. Run the python exe
 4. Change paths to match your
 system
-5. Run the VI
+5. Start VI
